8b24ffd88d058be176bccfb7f83a4f6faab1f0a1
Grzegorz Ciezkowski <grzegorz.ciezkowski@gmail.com>
2025-02-26 19:52:47 +0100

    Add benchmarks to redis

--- test/benchmarks/cnfqserver_bench_test.go
+++ test/benchmarks/cnfqserver_bench_test.go
@@ -17,24 +17,24 @@ func Benchmark_CNFQServer_Push(b *testing.B) {
 
 func Benchmark_CNFQServer_Pop(b *testing.B) {
 	client := queueclient.NewClient("http://localhost:8080")
-	for i := 0; i < b.N; i++ {
-		_ = client.Push("a", []byte("a"))
+	for i := 0; i < 1000; i++ {
+		_ = client.Push("b", []byte("a"))
 	}
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_, _ = client.Peek("a")
+		_, _ = client.Pop("b")
 	}
 }
 
 func Benchmark_CNFQServer_Peek(b *testing.B) {
 	client := queueclient.NewClient("http://localhost:8080")
-	for i := 0; i < b.N; i++ {
-		_ = client.Push("a", []byte("a"))
+	for i := 0; i < 1000; i++ {
+		_ = client.Push("c", []byte("a"))
 	}
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_, _ = client.Peek("a")
+		_, _ = client.Peek("c")
 	}
 }
