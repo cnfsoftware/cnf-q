2e533dbf49abca33e3679ab67700cda76cbf93aa
Grzegorz Ciezkowski <grzegorz.ciezkowski@gmail.com>
2025-02-27 15:34:30 +0100

    Add configuration layer

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"cnf-q/pkg/queueservice"
 	"log"
+	"os"
 )
 
 func main() {
-	qs := queueservice.NewQueueService()
+	qs := queueservice.NewQueueService(
+		queueservice.WithPort(os.Getenv("CNF_Q_PORT")),
+		queueservice.WithAccessToken(os.Getenv("CNF_Q_ACCESS_TOKEN")))
 	if err := qs.Run(); err != nil {
 		log.Fatalf("error starting queue service: %v", err)
 	}
--- pkg/queueservice/service.go
+++ pkg/queueservice/service.go
@@ -3,28 +3,74 @@ package queueservice
 import (
 	"cnf-q/pkg/queue"
 	"github.com/gin-gonic/gin"
-	"sync"
+	"net/http"
 )
 
 type QueueService struct {
-	manager    *queue.QueueManager
-	bufferPool *sync.Pool
+	manager     *queue.QueueManager
+	port        string
+	accessToken string
 }
 
-func NewQueueService() *QueueService {
-	return &QueueService{
+type Option func(*QueueService) *QueueService
+
+func NewQueueService(opts ...Option) *QueueService {
+	srv := &QueueService{
 		manager: queue.NewQueueManager(),
+		port:    "8080",
+	}
+
+	for _, opt := range opts {
+		srv = opt(srv)
+	}
+
+	return srv
+}
+
+func WithPort(port string) Option {
+	return func(srv *QueueService) *QueueService {
+		if port == "" {
+			return srv
+		}
+
+		srv.port = port
+		return srv
+	}
+}
+
+func WithAccessToken(accessToken string) Option {
+	return func(srv *QueueService) *QueueService {
+		if accessToken == "" {
+			return srv
+		}
+
+		srv.accessToken = accessToken
+		return srv
 	}
 }
 
 func (qs *QueueService) Run() error {
 	gin.SetMode(gin.ReleaseMode)
-	r := gin.New()
+	r := gin.Default()
+
+	if qs.accessToken != "" {
+		r.Use(qs.accessTokenMiddleware())
+	}
 
 	r.POST("/queue/:name/push", qs.pushHandler)
 	r.GET("/queue/:name/pop", qs.popHandler)
 	r.GET("/queue/:name/peek", qs.peekHandler)
 	r.GET("/queues", qs.listQueuesHandler)
 
-	return r.Run(":8080")
+	return r.Run(":" + qs.port)
+}
+
+func (qs *QueueService) accessTokenMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.GetHeader("X-Auth-Token") != qs.accessToken {
+			c.JSON(http.StatusForbidden, gin.H{"error": "access token is incorrect"})
+			c.Abort()
+			return
+		}
+	}
 }
